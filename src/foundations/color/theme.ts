4eb66ddd6c0129149fa9dad24bc5f3f998579e97
Joe Cocco <cocco3@gmail.com>
2024-10-25 11:57:35 -0700

    color

--- src/foundations/color/theme.ts
+++ src/foundations/color/theme.ts
@@ -1,21 +1,31 @@
 import { primitives } from './primitive';
 
-const { neutral, blue } = primitives;
+const { gray, teal, red, yellow, green } = primitives;
 
 export type Theme = typeof defaultTheme;
 
 export const defaultTheme = {
   text: {
-    primary: neutral['900'],
-    secondary: neutral['800'],
-    link: blue['900'],
+    primary: gray['900'],
+    secondary: gray['700'],
+    tertiary: gray['600'],
+    disabled: gray['500'],
+    placeholder: gray['500'],
+    error: red['600'],
+    warning: yellow['600'],
+    success: green['600'],
   },
 };
 
 export const darkTheme: Theme = {
   text: {
-    primary: neutral['00'],
-    secondary: neutral['50'],
-    link: blue['100'],
+    primary: gray['50'],
+    secondary: gray['300'],
+    tertiary: gray['400'],
+    disabled: gray['500'],
+    placeholder: gray['400'],
+    error: red['400'],
+    warning: yellow['400'],
+    success: green['400'],
   },
 };
