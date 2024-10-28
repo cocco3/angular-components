ef46da86d729c46bfad3df6750ae22c1f728baa9
Joe Cocco <cocco3@gmail.com>
2024-10-28 12:06:47 -0700

    updates

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,12 @@ import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@chromatic-com/storybook',
+    '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-links',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-themes',
   ],
   framework: {
     name: '@storybook/angular',
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,16 +1,31 @@
 import type { Preview } from '@storybook/angular';
 import { setCompodocJson } from '@storybook/addon-docs/angular';
+import { withThemeByClassName } from '@storybook/addon-themes';
 import docJson from '../documentation.json';
 setCompodocJson(docJson);
 
 const preview: Preview = {
+  decorators: [
+    withThemeByClassName({
+      defaultTheme: 'light',
+      themes: {
+        light: 'theme-light',
+        dark: 'theme-dark',
+      },
+    }),
+  ],
+
   parameters: {
+    backgrounds: { disable: true },
+
     docs: {
       canvas: { sourceState: 'shown' },
     },
 
     controls: {
       expanded: true,
+      hideNoControlsWarning: true,
+      sort: 'requiredFirst',
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
