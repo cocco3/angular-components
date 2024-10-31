61fb1d8ce57e42aaa120819a5e1876ed55e40686
Joe Cocco <cocco3@gmail.com>
2024-10-30 17:26:11 -0700

    color primitives

---
+++ src/foundations/color/primitiveColorsArray.ts
@@ -0,0 +1,9 @@
+import { primitives } from './primitive';
+
+export const primitiveColorsArray = [
+  { name: 'Gray', id: 'gray', colors: primitives.gray },
+  { name: 'Teal', id: 'teal', colors: primitives.teal },
+  { name: 'Red', id: 'red', colors: primitives.red },
+  { name: 'Green', id: 'green', colors: primitives.green },
+  { name: 'Yellow', id: 'yellow', colors: primitives.yellow },
+];
